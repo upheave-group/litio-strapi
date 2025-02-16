9f0ddfdc9735bc4059c69df416ea8e0bdf0f8796
Hasan Tfaily <hasan.tf.11@outlook.com>
2025-02-16 16:45:46 +0200

    config

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,20 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+
+  admin: {
+    autoOpen: false,
+    watchIgnoreFiles: ["*/private/*", "*/node_modules/*", "*/documentation/*"],
+  },
+  // Critical for memory management
+  cron: {
+    enabled: false,
+  },
+  graphql: {
+    enabled: false,
+    shadowCRUD: false,
   },
 });
